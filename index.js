2864b93cb29baeea8169a64cff73ee4653f02a1f
AlecStein44 <45604055+AlecStein44@users.noreply.github.com>
2019-06-09 16:17:51 -0700

    Update index.js

--- index.js
+++ index.js
@@ -133,7 +133,8 @@ app.post('/riflepost', (req, res) => {
 
 
     form.on('fileBegin', function (name, file){
-         cloudinary.uploader.upload(`${file.name}`,
+      file.path = `https://res.cloudinary.com/hugbqzfbt/image/upload/uploads/${file.name}`
+         cloudinary.uploader.upload(`${file.path}`,
          function(result) { console.log(result) })
     });
 
