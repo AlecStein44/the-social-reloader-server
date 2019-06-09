7b1eaa47a0192585cfbb275314c80db0226976cb
AlecStein44 <45604055+AlecStein44@users.noreply.github.com>
2019-06-09 15:42:06 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    CLOUDINARY_URL=cloudinary: process.env.CLOUDINARY_URL ||//876515715776955:jueehoj8eTKBLNAfCn4C_cddhbM@hugbqzfbt,
+    CLOUDINARY_URL: process.env.CLOUDINARY_URL ||//876515715776955:jueehoj8eTKBLNAfCn4C_cddhbM@hugbqzfbt,
     DB_URL: process.env.DATABASE_URL || 'postgres://apsqpzwiuomibe:b4e3b0df2e6d19c1d354c3d06f0f2115adea1a440b2953528ef1b3550e76d431@ec2-107-20-230-70.compute-1.amazonaws.com:5432/db2r6n8lnpjao2',
 }
