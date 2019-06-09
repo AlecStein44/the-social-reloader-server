27907877a777b2e53b0701ed279db15e12f12e8f
AlecStein44 <45604055+AlecStein44@users.noreply.github.com>
2019-06-09 14:49:38 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/db2r6n8lnpjao2',
+    DB_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/The-Social-Reloader',
 }
